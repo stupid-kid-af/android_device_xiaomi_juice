0b1c8d01d2b5c3dd2a00a0a302bf7e54fb9fceb5
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-22 13:42:19 +0530

    lineage

--- lineage_juice.mk
+++ lineage_juice.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
@@ -16,7 +16,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := lineage_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
